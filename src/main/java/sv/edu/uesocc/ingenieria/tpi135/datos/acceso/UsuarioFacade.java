ea2f8f3894443cc60a28fa65f9d5729201320643
luis21-GA <48488166+luis21-GA@users.noreply.github.com>
2019-03-15 18:27:48 -0600

    Update UsuarioFacade.java

--- src/main/java/sv/edu/uesocc/ingenieria/tpi135/datos/acceso/UsuarioFacade.java
+++ src/main/java/sv/edu/uesocc/ingenieria/tpi135/datos/acceso/UsuarioFacade.java
@@ -5,6 +5,7 @@
  */
 package sv.edu.uesocc.ingenieria.tpi135.datos.acceso;
 
+import javax.ejb.LocalBean;
 import javax.ejb.Stateless;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
@@ -14,6 +15,7 @@ import sv.edu.uesocc.ingenieria.tpi135.datos.definicion.Usuario;
  *
  * @author jonahdz
  */
+@LocalBean
 @Stateless
 public class UsuarioFacade extends AbstractFacade<Usuario> implements UsuarioFacadeLocal {
 
