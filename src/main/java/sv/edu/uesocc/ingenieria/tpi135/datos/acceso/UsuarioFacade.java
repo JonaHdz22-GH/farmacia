6828984f00c26932693be4fe5fbe6be01b2fc11e
luis21-GA <48488166+luis21-GA@users.noreply.github.com>
2019-03-15 18:29:46 -0600

    Update UsuarioFacade.java
    Validación de los métodos del abstractFace y creación de los genericos para los accesos.

--- src/main/java/sv/edu/uesocc/ingenieria/tpi135/datos/acceso/UsuarioFacade.java
+++ src/main/java/sv/edu/uesocc/ingenieria/tpi135/datos/acceso/UsuarioFacade.java
@@ -25,6 +25,7 @@ public class UsuarioFacade extends AbstractFacade<Usuario> implements UsuarioFac
     @Override
     protected EntityManager getEntityManager() {
         return em;
+        
     }
 
     public UsuarioFacade() {
