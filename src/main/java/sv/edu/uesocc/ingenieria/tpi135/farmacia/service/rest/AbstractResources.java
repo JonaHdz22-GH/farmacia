2480b98aa000a69830f9ee73d1a3b8413c4d311e
JonathanGarcía <jonahdz@jonahdz.jonahdz22.com>
2019-06-02 14:59:27 -0600

    Prueba de Commit Para Construir en Jenkins 3

--- src/main/java/sv/edu/uesocc/ingenieria/tpi135/farmacia/service/rest/AbstractResources.java
+++ src/main/java/sv/edu/uesocc/ingenieria/tpi135/farmacia/service/rest/AbstractResources.java
@@ -2,7 +2,7 @@
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
-    PRUEBA DE COMMIT PARA CONSTRUIR EN JENKINS 2
+    PRUEBA DE COMMIT PARA CONSTRUIR EN JENKINS 3
  */
 package sv.edu.uesocc.ingenieria.tpi135.farmacia.service.rest;
 
